681c466fb918c8198e809bd7113254f764b01ec5
JackAhn <an71195646@gmail.com>
2024-11-27 23:35:11 +0900

    [Feature] Apple 로그인 구현

--- gandang-fe/lib/view/login_view.dart
+++ gandang-fe/lib/view/login_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gandang/data/apple_login_api.dart';
 import 'package:gandang/data/google_login_api.dart';
 import 'package:gandang/data/kakao_login_api.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class LoginView extends ConsumerWidget {
   const LoginView({super.key});
@@ -27,7 +29,12 @@ class LoginView extends ConsumerWidget {
                   print('구글 액세스 토큰 정보 : ${auth.accessToken}');
                 }
               },
-                  child: const Text('구글'))
+                  child: const Text('구글')),
+              FilledButton(onPressed: () async{
+                AuthorizationCredentialAppleID user = await AppleLoginApi.instance.signInWithApple();
+                print('애플 액세스 토큰 정보 : ${user.identityToken}');
+              },
+                  child: const Text('Apple'))
             ],
           )
       ),
