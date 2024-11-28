d9159e07f749a0ef32e39b816dbfbf0383f29569
J-1ac <tkdwns26@naver.com>
2024-11-29 01:10:20 +0900

    feat: 필요한 요청, 응답 객체 생성

---
+++ be/gandang/src/main/java/gandang/pm/dto/NearestBicycleStationResponseDto.java
@@ -0,0 +1,17 @@
+package gandang.pm.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class NearestBicycleStationResponseDto {
+    private String stationName;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private Double distanceInMeters;  // 사용자 위치로부터의 거리(미터)
+}
