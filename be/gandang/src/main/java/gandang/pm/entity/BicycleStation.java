3b96a08f2d2d3457582ae970f0898a54b0517c53
J-1ac <tkdwns26@naver.com>
2024-11-29 17:09:09 +0900

    style: import 최적화 및 코드 컨벤션 적용

--- be/gandang/src/main/java/gandang/pm/entity/BicycleStation.java
+++ be/gandang/src/main/java/gandang/pm/entity/BicycleStation.java
@@ -1,12 +1,16 @@
 package gandang.pm.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
-import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
